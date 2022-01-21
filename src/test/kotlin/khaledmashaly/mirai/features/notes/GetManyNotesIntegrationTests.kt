e99c3e8b42eb018589a1019c3d4ac6b2bbf0e9c0
Khaled Mashaly <kmmashaly@gmail.com>
2022-01-21 21:20:45 +0200

    Create a basic test for "get many notes" endpoint

---
+++ src/test/kotlin/khaledmashaly/mirai/features/notes/GetManyNotesIntegrationTests.kt
@@ -0,0 +1,27 @@
+package khaledmashaly.mirai.features.notes
+
+import khaledmashaly.mirai.MiraiApplicationTests
+import org.hamcrest.Matchers.hasSize
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@DisplayName("Note endpoints")
+class GetManyNotesIntegrationTests: MiraiApplicationTests() {
+    @Autowired
+    lateinit var testClient: WebTestClient
+
+    @Test
+    @DisplayName("Returns a list of notes")
+    fun `Returns a list of notes`() {
+        testClient
+            .get()
+            .uri("/note")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+                .jsonPath("$").isArray
+                .jsonPath("$").value(hasSize<Any>(3))
+    }
+}
