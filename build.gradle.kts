3794992f7ebecf7248579ea87c37919381e3020b
Khaled Mashaly <kmmashaly@gmail.com>
2022-01-26 22:38:12 +0200

    Use dev profile for bootRun gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
 	id("org.springframework.boot") version "2.6.2"
@@ -48,4 +49,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<BootRun> {
+	args = listOf("--spring.profiles.active=dev")
 }
