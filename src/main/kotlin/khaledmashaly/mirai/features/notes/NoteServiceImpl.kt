bd4819f47f535e64f157c1c94f9027f5c7db1300
Khaled Mashaly <kmmashaly@gmail.com>
2022-01-30 22:44:40 +0200

    Return static list of notes to pass test

--- src/main/kotlin/khaledmashaly/mirai/features/notes/NoteServiceImpl.kt
+++ src/main/kotlin/khaledmashaly/mirai/features/notes/NoteServiceImpl.kt
@@ -2,11 +2,21 @@ package khaledmashaly.mirai.features.notes
 
 import lombok.extern.slf4j.Slf4j
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 
 @Slf4j
 @Service
 class NoteServiceImpl(
     val noteRepo: NoteRepo
 ): NoteService {
-    override fun getManyNotes() = noteRepo.findAll()
+    override fun getManyNotes() = Flux.just(
+        Note(
+            title = "First note",
+            description = "first description"
+        ),
+        Note(
+            title = "second note",
+            description = "second description"
+        )
+    )
 }
