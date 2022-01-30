4d8cf57c46377a6b55f69b58b2b48da582f3a961
Khaled Mashaly <kmmashaly@gmail.com>
2022-01-30 22:37:55 +0200

    Use a single expression function for getManyNotes

--- src/main/kotlin/khaledmashaly/mirai/features/notes/NoteServiceImpl.kt
+++ src/main/kotlin/khaledmashaly/mirai/features/notes/NoteServiceImpl.kt
@@ -2,15 +2,11 @@ package khaledmashaly.mirai.features.notes
 
 import lombok.extern.slf4j.Slf4j
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Flux
 
 @Slf4j
 @Service
 class NoteServiceImpl(
     val noteRepo: NoteRepo
 ): NoteService {
-
-    override fun getManyNotes(): Flux<Note> {
-        return noteRepo.findAll()
-    }
+    override fun getManyNotes() = noteRepo.findAll()
 }
