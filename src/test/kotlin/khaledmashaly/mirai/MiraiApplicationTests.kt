bd7e465ed2801082bb510f931f6f38c32766b7fc
Khaled Mashaly <kmmashaly@gmail.com>
2022-01-31 22:33:49 +0200

    Move test client declaration to parent test class

--- src/test/kotlin/khaledmashaly/mirai/MiraiApplicationTests.kt
+++ src/test/kotlin/khaledmashaly/mirai/MiraiApplicationTests.kt
@@ -1,10 +1,15 @@
 package khaledmashaly.mirai
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureWebTestClient
-class MiraiApplicationTests
+class MiraiApplicationTests {
+    @Autowired
+    lateinit var testClient: WebTestClient
+}
--- src/test/kotlin/khaledmashaly/mirai/features/notes/GetManyNotesIntegrationTests.kt
+++ src/test/kotlin/khaledmashaly/mirai/features/notes/GetManyNotesIntegrationTests.kt
@@ -4,14 +4,9 @@ import khaledmashaly.mirai.MiraiApplicationTests
 import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.web.reactive.server.WebTestClient
 
 @DisplayName("Note endpoints")
 class GetManyNotesIntegrationTests: MiraiApplicationTests() {
-    @Autowired
-    lateinit var testClient: WebTestClient
-
     @Test
     @DisplayName("Returns a list of notes")
     fun `Returns a list of notes`() {
