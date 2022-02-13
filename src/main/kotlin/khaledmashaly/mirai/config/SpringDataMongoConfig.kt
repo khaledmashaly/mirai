c2b1d48f8e3b0720dea29a8c4ab5e7c655220076
Khaled Mashaly <kmmashaly@gmail.com>
2022-02-13 22:03:19 +0200

    Enable Spring Data Mongo auditing

---
+++ src/main/kotlin/khaledmashaly/mirai/config/SpringDataMongoConfig.kt
@@ -0,0 +1,8 @@
+package khaledmashaly.mirai.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
+
+@Configuration
+@EnableReactiveMongoAuditing
+class SpringDataMongoConfig
