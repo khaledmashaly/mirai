e1ddec0fe2e148610fe6bf61598a0a65c77162d9
Khaled Mashaly <kmmashaly@gmail.com>
2022-02-13 22:58:45 +0200

    Add more tests for "create note" endpoint

--- src/test/kotlin/khaledmashaly/mirai/features/notes/CreateNoteIntegrationTests.kt
+++ src/test/kotlin/khaledmashaly/mirai/features/notes/CreateNoteIntegrationTests.kt
@@ -1,19 +1,24 @@
 package khaledmashaly.mirai.features.notes
 
 import khaledmashaly.mirai.MiraiApplicationTests
-import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.startsWith
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.http.MediaType
 import reactor.test.StepVerifier
+import java.time.LocalDate
 
 @DisplayName("Create a note")
 class CreateNoteIntegrationTests: MiraiApplicationTests() {
     @Test
     @DisplayName("Adds a new note")
-    fun `Adds a new note test`() {
-        val request = """{"title": "new note title","description": "new note description"}"""
+    fun addNewNoteTest() {
+        val request = """{
+            |"title": "new note title",
+            |"description": "new note description"
+        |}""".trimMargin()
 
         testClient
             .post()
@@ -23,10 +28,8 @@ class CreateNoteIntegrationTests: MiraiApplicationTests() {
             .exchange()
                 .expectStatus().isCreated
                 .expectBody()
-                    .jsonPath("$.length()").value(`is`(6))
+                    .jsonPath("$.length()").value(equalTo(6))
                     .jsonPath("$.id").exists()
-                    .jsonPath("$.title").value(`is`("new note title"))
-                    .jsonPath("$.description").value(`is`("new note description"))
 
         val eventualCount = mongoOps.count(Query(), Note::class.java)
 
@@ -34,5 +37,46 @@ class CreateNoteIntegrationTests: MiraiApplicationTests() {
             .create(eventualCount)
             .expectNext(1)
             .verifyComplete()
+    }
+
+    @Test
+    @DisplayName("Sets title and description")
+    fun setTitleDescriptionTest() {
+        val request = """{
+            |"title": "new note title",
+            |"description": "new note description"
+        |}""".trimMargin()
+
+        testClient
+            .post()
+                .uri("/v1/note")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+            .exchange()
+                .expectStatus().isCreated
+                .expectBody()
+                    .jsonPath("$.title").value(equalTo("new note title"))
+                    .jsonPath("$.description").value(equalTo("new note description"))
+    }
+
+    @Test
+    @DisplayName("Sets auditing properties: createdDate, lastModifiedDate, version")
+    fun setAuditingProperties() {
+        val request = """{
+            |"title": "new note title",
+            |"description": "new note description"
+        |}""".trimMargin()
+
+        testClient
+            .post()
+                .uri("/v1/note")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(request)
+            .exchange()
+                .expectStatus().isCreated
+                .expectBody()
+                    .jsonPath("$.createdDate").value(startsWith(LocalDate.now().toString()))
+                    .jsonPath("$.lastModifiedDate").value(startsWith(LocalDate.now().toString()))
+                    .jsonPath("$.version").value(equalTo(0))
     }
 }
