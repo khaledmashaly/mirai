c32cf2c26a79e551522a05f0cfedb76aeb5d4a10
Khaled Mashaly <kmmashaly@gmail.com>
2022-02-27 21:55:51 +0200

    Create a method to return a paginated response for notes

--- src/main/kotlin/khaledmashaly/mirai/features/notes/NoteRepo.kt
+++ src/main/kotlin/khaledmashaly/mirai/features/notes/NoteRepo.kt
@@ -1,6 +1,10 @@
 package khaledmashaly.mirai.features.notes
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 import java.util.*
 
-interface NoteRepo: ReactiveMongoRepository<Note, UUID>
+interface NoteRepo: ReactiveMongoRepository<Note, UUID> {
+    fun findAll(pageable: Pageable): Flux<Note>
+}
