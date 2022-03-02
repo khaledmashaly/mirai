a89e11a7afc21024a67a6d6e2993e2db961ed7fd
Khaled Mashaly <kmmashaly@gmail.com>
2022-03-02 22:10:22 +0200

    Add more tests for get many notes endpoint

--- src/test/kotlin/khaledmashaly/mirai/features/notes/GetManyNotesIntegrationTests.kt
+++ src/test/kotlin/khaledmashaly/mirai/features/notes/GetManyNotesIntegrationTests.kt
@@ -2,6 +2,7 @@ package khaledmashaly.mirai.features.notes
 
 import khaledmashaly.mirai.MiraiApplicationTests
 import khaledmashaly.mirai.features.notes.mocks.NOTE_MOCKS
+import org.hamcrest.Matchers.contains
 import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -25,6 +26,81 @@ class GetManyNotesIntegrationTests: MiraiApplicationTests() {
                 .expectStatus().isOk
                 .expectBody()
                     .jsonPath("$").isArray
-                    .jsonPath("$").value(hasSize<Any>(2))
+                    .jsonPath("$").value(hasSize<Any>(5))
+    }
+
+    @Test
+    @DisplayName("Sorts notes by createdDate descending")
+    fun sortNotesCreateDateDescending() {
+        testClient
+            .get()
+                .uri("/v1/note")
+            .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                    .jsonPath("$").isArray
+                    .jsonPath("$[*].id", contains(
+                        NOTE_MOCKS[0].id,
+                        NOTE_MOCKS[1].id,
+                        NOTE_MOCKS[2].id,
+                        NOTE_MOCKS[3].id,
+                        NOTE_MOCKS[4].id
+                    ))
+    }
+
+    @Test
+    @DisplayName("Adjusts response size using `size` paramter")
+    fun adjustResponseSizeUsingSizeParameter() {
+        testClient
+            .get()
+                .uri("/v1/note?size=3")
+            .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                .jsonPath("$").isArray
+                .jsonPath("$").value(hasSize<Any>(3))
+    }
+
+    @Test
+    @DisplayName("Adjusts response offset using `page` paramter")
+    fun adjustResponseOffsetUsingPageParameter() {
+        testClient
+            .get()
+                .uri("/v1/note?page=1")
+            .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                    .jsonPath("$").isArray
+                    .jsonPath("$").value(hasSize<Any>(0))
+    }
+
+    @Test
+    @DisplayName("Requires `page` parameter to be a non-negative integer")
+    fun requirePageParameterNonNegativeInteger() {
+        testClient
+            .get()
+                .uri("/v1/note?page=-1")
+            .exchange()
+                .expectStatus().isBadRequest
+    }
+
+    @Test
+    @DisplayName("Requires `size` parameter to be a positive integer")
+    fun requireSizeParameterPositiveInteger() {
+        testClient
+            .get()
+                .uri("/v1/note?size=0")
+            .exchange()
+                .expectStatus().isBadRequest
+    }
+
+    @Test
+    @DisplayName("Requires `size` parameter to be less than 101")
+    fun requireSizeParameterMax() {
+        testClient
+            .get()
+                .uri("/v1/note?size=101")
+            .exchange()
+                .expectStatus().isBadRequest
     }
 }
--- src/test/kotlin/khaledmashaly/mirai/features/notes/mocks/NoteMocks.kt
+++ src/test/kotlin/khaledmashaly/mirai/features/notes/mocks/NoteMocks.kt
@@ -23,5 +23,32 @@ val NOTE_MOCKS = listOf(
         version = 0,
         title = "title-2",
         description = "description-2",
+    ),
+
+    Note(
+        id = UUID.fromString("2784c59e-21e2-4b1f-8c23-d165af5319a4"),
+        createdDate = TODAY.minusDays(2),
+        lastModifiedDate = TODAY.minusDays(2),
+        version = 0,
+        title = "title-3",
+        description = "description-3",
+    ),
+
+    Note(
+        id = UUID.fromString("dd590182-1c18-4db8-9a6a-86586cb58f82"),
+        createdDate = TODAY.minusDays(3),
+        lastModifiedDate = TODAY.minusDays(3),
+        version = 0,
+        title = "title-4",
+        description = "description-4",
+    ),
+
+    Note(
+        id = UUID.fromString("b6a792fd-1a0a-44ac-b11d-c447bb7b5158"),
+        createdDate = TODAY.minusDays(4),
+        lastModifiedDate = TODAY.minusDays(4),
+        version = 0,
+        title = "title-5",
+        description = "description-5",
     )
 )
